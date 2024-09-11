aaa4d808c1b3d81cef9dec5dc3075d881741acdf
sumit-2006 <129647800+sumit-2006@users.noreply.github.com>
2024-09-11 17:20:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Externalize dependencies that you want to exclude from the bundle
+      external: ['react-router-dom'], // Add other external modules if necessary
+    },
+  },
 })
